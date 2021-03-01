3cd2413f30c9d3af5f9f4d92db9dd75eb1627350
Lorelei M <lorelei.miyamura@gmail.com>
2021-03-01 09:25:59 -0800

    theme name listener working

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,21 @@
 import style from "./index.module.scss";
 import DownloadButton from "../download";
+import React, { useState } from "react";
 
 export default function Home() {
+
+  //this is the themeName listener, that changes on input keyup
+  const [themeName, setThemeName] = useState("default");
+
+  function themeNameListener(e) {
+    setThemeName(e.target.value);
+  }
 
   return (
     <div className={style.heroContainer}>
       <h1>generateTheme<span className={style.pink}>( )</span></h1>
-      <DownloadButton themeNameProp={"hi"}/>
+      <input type="text" onChange={themeNameListener}></input>
+      <DownloadButton themeNameProp={themeName}/>
     </div>
   );
 }
