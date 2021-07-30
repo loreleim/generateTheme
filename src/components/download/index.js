69e82025c97b91a2205acfe1652d60b582288d44
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-07-30 12:07:16 -0700

    replace blocks with posttypes

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -73,6 +73,66 @@ if ( get_query_var('pagename') ) {
 
 <?php get_footer(); ?>`
 
+  var defaultPosttype = 
+`<?php 
+
+// Register Custom Post Type
+function custom_posttype_test() {
+
+  $labels = array(
+    'name'                => _x( 'Tests', 'Post Type General Name', 'text_domain' ),
+    'singular_name'       => _x( 'Test', 'Post Type Singular Name', 'text_domain' ),
+    'menu_name'           => __( 'Tests', 'text_domain' ),
+    'parent_item_colon'   => __( 'Parent Test:', 'text_domain' ),
+    'all_items'           => __( 'All Tests', 'text_domain' ),
+    'view_item'           => __( 'View Test', 'text_domain' ),
+    'add_new_item'        => __( 'Add New Test', 'text_domain' ),
+    'add_new'             => __( 'Add New', 'text_domain' ),
+    'edit_item'           => __( 'Edit Test', 'text_domain' ),
+    'update_item'         => __( 'Update Test', 'text_domain' ),
+    'search_items'        => __( 'Search Test', 'text_domain' ),
+    'not_found'           => __( 'Not found', 'text_domain' ),
+    'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+  );
+
+  $rewrite = array(
+    'slug'                => 'tests',
+    'with_front'          => true,
+    'pages'               => true,
+    'feeds'               => true,
+  );
+
+  $args = array(
+    'label'               => __( 'Tests', 'text_domain' ),
+    'description'         => __( 'Test Type Description', 'text_domain' ),
+    'labels'              => $labels,
+    'supports'            => array( 'title','editor', 'custom-fields'),
+    'taxonomies'          => array(),
+    'hierarchical'        => false,
+    'public'              => true,
+    'show_ui'             => true,
+    'show_in_menu'        => true,
+    'show_in_nav_menus'   => true,
+    'show_in_admin_bar'   => true,
+    'menu_position'       => 5,
+    'menu_icon'           => 'dashicons-format-video',
+    'can_export'          => true,
+    'has_archive'         => false,
+    'publicly_queryable'  => true,
+    'show_in_rest'      => true,
+    'rewrite'             => $rewrite,
+    'capability_type'     => 'post'
+  );
+
+  register_post_type( 'test', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'custom_posttype_test', 0 );
+?>
+`
+
   function generateZip() {
     var zip = new JSZip();
 
@@ -92,13 +152,11 @@ if ( get_query_var('pagename') ) {
     /*LIBRARY FOLDER*/
 
     //library dir
-    var folder1 = zip.folder("library");
+    var libraryFolder = zip.folder("library");
 
-    //blocks dir
-    var blocksFolder = folder1.folder("blocks");
-    blocksFolder.file("Hello.txt", "Hello World\n");
-
-    //blocks - gutenberg block templates
+    //posttypes dir
+    var posttypesFolder = libraryFolder.folder("posttypes");
+    posttypesFolder.file("posttype_test.php", defaultPosttype);
 
     /*PAGES FOLDER*/
 
